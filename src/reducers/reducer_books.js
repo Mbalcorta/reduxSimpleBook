783f4f8fcd7a40b6c4f30ebccdf278ce60a78a15
Mateo Balcorta <misterrattail@gmail.com>
2018-04-22 16:33:41 -0700

    Add create reducer function for books.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'Js: The Good Parts', pages: 101},
+        {title: 'Js: one', pages: 39},
+        {title: 'Js: Two', pages: 2},
+        {title: 'Js: Three', pages: 1}
+    ]
+}
